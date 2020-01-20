69eb6be9f8901b196ce1f6d0cd2f3c82df7f3d7b
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-20 16:41:20 +0530

    Add files via upload

---
+++ Exception/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(num / 2);
+                try
+                {
+                    int[] a = new int[] { 1, 2, 3 };
+                    Console.WriteLine(a[3]);
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message + " " + e.Source + " " + e.StackTrace);
+                }
+                
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message + " " + e.Source + " " + e.StackTrace);
+
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message + " " + e.Source + " " + e.StackTrace);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " " + e.Source + " " + e.StackTrace);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
