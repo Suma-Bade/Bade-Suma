4e42189ca8a7a9ddcfd7ed29c32481035ef131de
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-20 16:44:25 +0530

    Add files via upload

---
+++ FirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to CTS");
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();//reads value from input
+            Console.WriteLine("HEllo " + name);
+            Console.ReadKey();//to read console output
+        }
+    }
+}
