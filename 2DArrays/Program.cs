a0331249f5853df6321d4b265e71a37ad3e09d91
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-20 16:24:35 +0530

    Add files via upload

---
+++ 2DArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] marks = new int[5, 3];
+            for(int row=0;row<5;row++)
+            {
+                for(int column=0;column<3;column++)
+                {
+                    marks[row, column] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int row = 0; row < 5; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    Console.WriteLine(""+marks[row, column]);
+                }
+                Console.WriteLine();
+            }
+            foreach (int x in marks)
+                Console.WriteLine(x);
+            Console.ReadKey();
+
+        }
+    }
+}
