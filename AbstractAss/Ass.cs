823475b324af6b0ec4ec98892f5bd7e78a5c16bc
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-20 16:25:58 +0530

    Add files via upload

---
+++ AbstractAss/Ass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAss
+{
+    class Ass
+    {
+        public static void Main(String[] args)
+        {
+            Undergrad ug = new Undergrad();
+            Console.WriteLine("enter grade");
+            int grade = int.Parse(Console.ReadLine());
+            Console.WriteLine(""+( ug.Ispassed(grade)));
+            Grad g = new Grad();
+            Console.WriteLine("enter grade");
+            int grade2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("" + (g.Ispassed(grade2)));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractAss/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAss
+{
+  public  abstract  class Student
+    {
+       protected string name;
+        protected int studentId;
+        protected int grade;
+        public abstract Boolean Ispassed(int grade);
+
+    }
+    public class Undergrad:Student
+    {
+        public override bool Ispassed(int grade)
+        {
+            
+            if (grade > 70)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    public class Grad:Student
+    {
+        public override bool Ispassed(int grade)
+        {
+            if (grade > 80)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
