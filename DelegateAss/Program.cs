188a93923748c47f82e659a835aa679938e50342
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-20 16:37:38 +0530

    Add files via upload

---
+++ DelegateAss/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAss
+{
+    class Trainee
+    {
+        int id;
+        string name;
+        int projectId;
+        public Trainee(int id,string name,int projectId)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.ProjectId = projectId;
+
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int ProjectId { get => projectId; set => projectId = value; }
+        public override string ToString()
+        {
+            return "Id is "+this.id + "\nName is " + this.name + "\nProject id " + this.projectId;
+        }
+    }
+}
---
+++ DelegateAss/Traine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAss
+{
+    class Trainee1
+    {
+        public static void Main(string[] args)
+        {
+            List<Trainee> lst = new List<Trainee>();
+            Console.WriteLine("enter no of trainees");
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine("enter id");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter name");
+                string name = Console.ReadLine();
+                Console.WriteLine("enter project id");
+                int projectId = int.Parse(Console.ReadLine());
+                lst.Add(new Trainee(id, name, projectId));
+            }
+            List<Trainee> pi = lst.FindAll(y => y.ProjectId != -1);
+            foreach(Trainee s in pi)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
