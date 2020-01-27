2d60c39d3e01db9ed851ff071a2f197037f16de4
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-27 09:30:04 +0530

    Add files via upload

---
+++ Interfaces/Bank.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface Iloan
+    {
+         void getInterest();//public by default
+    }
+    interface IGloan:Iloan//interface supports inheritance
+    {
+        void getGoldInterest();
+    }
+    interface IDmat
+    {
+        void displayDemat();
+    }
+  public abstract class Bank
+    {
+        protected string bName;
+        protected string location;
+        public Bank(string bname,string location)
+        {
+            this.bName = bname;
+            this.location = location;
+        }
+    }
+    public class SBI:Bank,IGloan
+    {
+        string bManager;
+        public SBI(string bname,string location,string bmanager):base(bname,location)
+        {
+            this.bManager = bmanager;
+        }
+        public void getInterest()
+        {
+            Console.WriteLine("SBI loan interset is 10%");
+        }
+        public void getGoldInterest()
+        {
+            Console.WriteLine("SBI gols loan intrest is 12%");
+        }
+        public void displayDetails()
+        {
+            Console.WriteLine(this.bName + " " + this.location + " " + this.bManager);
+        }
+
+    }
+    public class ICICI : Bank,Iloan,IDmat
+    {
+        string bHead;
+        public ICICI(string bname, string location, string bmanager) : base(bname, location)
+        {
+            this.bHead = bmanager;
+        }
+        public void displayDemat()
+        {
+            Console.WriteLine("ICICI can start demat accounts");
+        }
+        public void getInterest()
+        {
+            Console.WriteLine("ICICI loan interset is 17%");
+        }
+        public void displayDetails()
+        {
+            Console.WriteLine(this.bName + " " + this.location + " " + this.bHead);
+        }
+
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICICI chn = new ICICI("ICICI 001","Chennai","Rama");
+            SBI hyd = new SBI("SBI009", "hyderabad", "sita");
+            chn.displayDetails();
+            chn.getInterest();
+            hyd.displayDetails();
+            hyd.getInterest();
+            Console.ReadLine();
+        }
+    }
+}
