539cc4de521ccd20ff5064f2ac4c6f699516ab74
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-27 09:39:10 +0530

    Add files via upload

---
+++ Thread/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread1
+{
+    class ThreadClass
+    {
+        public void countEven()
+        {
+            for(int i=0;i<100;i+=2)
+            {
+                Console.WriteLine(i);
+                
+            }
+        }
+        public void countOdd()
+        {
+            for (int i = 1; i < 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+       
+    }
+}
---
+++ Thread/thread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Thread1
+{
+    class Threads
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("main starts");
+            ThreadClass obj = new ThreadClass();
+            Thread th = new Thread(obj.countEven);
+            Thread th1 = new Thread(obj.countOdd);
+            th.Priority = ThreadPriority.Lowest;
+            th1.Priority = ThreadPriority.Highest; 
+            th.Start();
+            th1.Start();
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
