8c081e40156b55fe7a2fd60631f548c2377f75fb
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-27 09:33:18 +0530

    Add files via upload

---
+++ OopsFeatures/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsFeatures
+{
+    class Calculator
+    {
+        int val1;
+        int val2;
+        public int Val1
+        {
+            get => val1;set => val1 = value;
+        }
+        public int Val2
+        {
+            get => val2; set => val2 = value;
+        }
+        public  void Calculator2(int val1,int val2)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+        public void Add(int num1,int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+        public void Swap(int num1,int num2)//call by value
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+        public void Swap(ref int num1,ref int num2)//call by ref
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+        public void Subtract(int num1,int num2,out int result)
+        {
+            result = num1 - num2;
+        }
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            cal.Add(x,y);
+            cal.Subtract(x, y, out int result);
+            Console.WriteLine(result);
+            cal.Swap(x, y);
+            cal.Swap(ref x, ref y);
+            Console.ReadKey();
+        }
+    }
+}
