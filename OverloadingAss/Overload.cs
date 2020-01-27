ce881e596e97c65bdc3d0108b25d0c6d592d1891
Suma-Bade <60084568+Suma-Bade@users.noreply.github.com>
2020-01-27 09:34:46 +0530

    Add files via upload

---
+++ OverloadingAss/Overload.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingAss
+{
+     class Delivery
+    { 
+        public   void DisplayDeliveryDetails(string bowler,string batsman)
+        {
+            Console.WriteLine("Bowler name : " + bowler + "\n Batsman Name : " + batsman);
+        }
+        public  void DisplayDeliveryDetails(long runs)
+        {
+            if(runs==6)
+                {
+                Console.WriteLine("It is a six");
+
+            }
+            else if(runs==4)
+            {
+                Console.WriteLine("it is a four");
+            }
+            else
+            {
+                Console.WriteLine("runs= " + runs);
+            }
+
+        }
+
+
+    }
+}
---
+++ OverloadingAss/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingAss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delivery d = new Delivery();
+            Console.WriteLine("1.Players details of the delivery\n2.Run details of the delivery");
+            Console.WriteLine("enter your choice");
+            int ch = int.Parse(Console.ReadLine());
+            switch(ch)
+            {
+                case 1:Console.WriteLine("enter bowler name");
+                    string bowler = Console.ReadLine();
+                    Console.WriteLine("enter batsmen name");
+                    string batsman = Console.ReadLine();
+                    d.DisplayDeliveryDetails(bowler,batsman);
+                    break;
+                case 2:Console.WriteLine("enter runs");
+                    int runs = int.Parse(Console.ReadLine());
+                    d.DisplayDeliveryDetails(runs);
+                    break;
+                default:
+                    Console.WriteLine("invalid");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
